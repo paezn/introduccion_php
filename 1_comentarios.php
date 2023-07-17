edc9143ee8af0cf28cbb1d08f3cafa6e3a1cc39b
paezn <npaez@colegioguanenta.edu.co>
2023-07-17 17:27:12 -0500

    Comentarios en php

---
+++ 1_comentarios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios en PHP</title>
+</head>
+<body>
+    <h1>Comentarios en PHP</h1>
+    <!--Formas de incluir codigo php-->
+    
+    <?php
+        //Comentario de una linea
+        /*Esto es un comentario de 
+        varias lineas */
+    ?>
+
+    <!--
+    <%
+        codigo php
+    %>
+
+    <script language="php">
+
+    </script>-->
+</body>
+</html>
